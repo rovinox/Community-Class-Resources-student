ea39fded7dc40feb33c96e716a6d3e322d83011e
Neaj Mahmud <neajmahmud.dev@gmail.com>
2024-09-16 17:04:47 -0400

    added link

--- learn-array/basics.js
+++ learn-array/basics.js
@@ -1,2 +1,10 @@
-// write you code here
+// 1. learn If statements
+// https://www.youtube.com/watch?v=PgUXiprlg1k&t=421s&ab_channel=BroCode
+
+// 2. learn about for loop
+//  https://www.youtube.com/watch?v=ZOQYIWLngSU&ab_channel=BroCode
+
+//3. learn advanced  way to work with array
 // https://www.youtube.com/watch?v=cDCzz8vJf3Y&ab_channel=EnvatoTuts%2B
+
+// write you code here
